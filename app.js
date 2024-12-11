a32a1bf01a610462b727a147938addc747bb617b
Abdul-Azeez060 <azeez160604@gmail.com>
2024-12-11 21:26:44 +0530

    ci workflows check

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ main()
 
 async function main() {
   try {
-    mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(process.env.MONGO_URL || "mongodb://");
   } catch (error) {
     next(new ExpressError("couldn't not connect to the database", 500));
   }
