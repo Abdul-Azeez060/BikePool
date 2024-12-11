62421fe7c6220f745db14a6f94b058391e4d9eea
Abdul-Azeez060 <azeez160604@gmail.com>
2024-12-12 00:05:35 +0530

    remove the name spaces

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const bookingsRouter = require("./routes/bookings");
 const usersRouter = require("./routes/user");
 const { checkAuthentication } = require("./middleware");
 const cookieParser = require("cookie-parser");
-
 const flash = require("connect-flash");
 const session = require("express-session");
-
 const port = 8080;
 const cluster = require("cluster");
 const os = require("os");
